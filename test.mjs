6ffebcaa9c209e9f8316950f0a3c6a6fe7104005
MinjiJeon <minji799@gmail.com>
2023-07-17 01:27:01 +0900

    큐 구현

--- test.mjs
+++ test.mjs
@@ -32,3 +32,10 @@ console.log(stack.pop().data);
 console.log(stack.pop().data);
 console.log(stack.pop().data);
 console.log(stack.pop().data);
+//큐
+//마트계산 : 먼저줄을선 손님이 먼저계산하는것이 큐
+//운영체제가 보낸 프로세스를 cpu가 순차적으로 처리 -->fifo스케줄링
+//구현 : head부터 차례대로 추가 그러나 삭제일 경우 head를 따고 계속 next로 이동한뒤 맨뒤에 node를 삭제해야함. 그래서 tail노드를 만들어 양방향으로 구현예정
+//enque: 데이터삽입
+//deque: 데이터 제거
+//front : 데이터 참조
