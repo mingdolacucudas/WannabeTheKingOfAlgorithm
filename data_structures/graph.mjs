7e1da73f2b34ac3edf1cd9859353e0593ea7b3de
MinjiJeon <minji799@gmail.com>
2024-06-29 11:56:47 +0900

    dfs

--- data_structures/graph.mjs
+++ data_structures/graph.mjs
@@ -18,7 +18,46 @@ class Vertex {
   }
 }
 
-let jake = new Vertex("jake");
-let amy = new Vertex("amy");
-let brian = new Vertex("brian");
-let kate = new Vertex("kate");
+function DFS(vertex, visited_vertices = new Set()) {
+  //vertex : 정점, visited : 방문한 정점
+  //깊이우선탐색
+
+  visited_vertices[vertex.value] = true; //현재 정점.
+  console.log(vertex.value);
+  for (let adjacent of vertex.adjacent_vertexs) {
+    if ((visited_vertices[adjacent.value] = true)) {
+      //이미방문
+      continue;
+    } else {
+      DFS(adjacent, visited_vertices);
+    }
+  }
+}
+
+//방문한 정점으로 저장하고 방문한 정점을 큐에 삽입
+// 큐에서 dequeue
+// dequeue한 정점의 인접한 정점을 순회.
+function BFS(vertex) {
+  let queue = []; //빈 큐 생성
+  let visited_vertices = []; //방문한 정점을 저장할 객체 생성
+  queue.push(vertex);
+
+  while (queue.length > 0) {
+    //큐가 빌때까지 반복
+    let current = queue.shift();
+    if (visited_vertices[current.value]) {
+      continue;
+    }
+    console.log(current.value);
+    visited_vertices[current.value] = true;
+    for (let adjacent of current.adjacent_vertexs) {
+      queue.push(adjacent);
+    }
+  }
+}
+//성능은 동일
+//DFS : 재귀함수로 구현
+//BFS : 큐로 구현
+// sns친구기능: 지인의 지인의 지인 - DFS / 친구의 친구들 - BFS -> bfs 가 적합
+// 그래프의 끝에있는 애들은 마지막에 도착.
+// 더 빨리 찾을 수 있는지 고민
